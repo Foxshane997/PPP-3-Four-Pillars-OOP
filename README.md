0ec976d65813ee13c54bbaefa2648378b5957435
“Foxshane997” <Foxshane997@gmail.com>
2024-08-19 19:58:44 -0700

    adding to repo

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Activity (ppp-3-the-four-pillars-of-object-oriented-programming)
 
-### https://digitalskills.instructure.com/courses/13320/pages/activity-ppp-3-the-four-pillars-of-object-oriented-programming?module_item_id=1909905# PPP-3-Four-Pillars-OOP
+### https://digitalskills.instructure.com/courses/13320/pages/activity-ppp-3-the-four-pillars-of-object-oriented-programming?module_item_id=1909905# 
---
+++ index.py
@@ -0,0 +1,41 @@
+class Person:
+  def __init__(self,in_name,in_age):
+    self.name = in_name
+    self.age = in_age
+      
+  def get_name(self):
+    return self.name
+
+class Zoo:
+  def __init__(self,name="Local Zoo"):
+    self.name = name
+    self.animals = []
+    self.customers = []
+
+  def add_animal(self, animal):
+    self.animals.append(animal)
+    print(f"{self.name} has a(n) {animal}")
+  
+  def add_animals(self, animals):
+    self.animals.extend(animals)
+    print(f"{self.name} has many animals")
+  
+  def add_customer(self, name):
+    self.customers.append(name)
+    print(f"{name} has entered {self.name}")
+
+  def remove_customer(self, name):
+    self.customers.remove(name)
+    print(f"{name} has left {self.name}")
+  
+  def visit_animals(self):
+    for a in self.animals:
+      print(f"visiting {a.get_name()}")
+      a.make_noise()
+      a.eat_food()
+
+class Animal:
+  def __init__(self,name):
+    self.name = name
+  def get_name(self):
+    return self.name
